51ae6a37deafa9dc24756cd9eb42df7a280c40ab
Ricardo Meireles <ricardomeireles205@gmail.com>
2020-11-04 02:50:45 +0000

    Added shake event to CanvasFragment On shake, o canvasErase é invocado e o telémovel vibra.
    Threshold e "maneira" de detectar o shake têm de ser ajustadas.

--- app/src/main/java/com/example/paint/CanvasFragment.java
+++ app/src/main/java/com/example/paint/CanvasFragment.java
@@ -5,9 +5,16 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -24,7 +31,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Random;
 
-public class CanvasFragment extends Fragment {
+public class CanvasFragment extends Fragment implements SensorEventListener {
     private static final String canvasLinesBundleKey = "0wskkf37ed";
 
     private PaintCanvas paintCanvas;
@@ -32,6 +39,13 @@ public class CanvasFragment extends Fragment {
     private GestureListener mGestureListener;
     private GestureDetector mGestureDetector;
 
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private boolean isAccelerometerSensorAvailable, itIsNotFirstTime = false;
+    private float currentX, currentY, currentZ, lastX, lastY, lastZ;
+    private float xDifference, yDifference, zDifference;
+    private float shakeThreshold = 7f;
+    private Vibrator vibrator;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,6 +56,18 @@ public class CanvasFragment extends Fragment {
         mGestureDetector.setOnDoubleTapListener(mGestureListener);
         paintCanvas = new PaintCanvas(getContext(), null, mGestureDetector);
         mGestureListener.setCanvas(paintCanvas);
+
+        vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            isAccelerometerSensorAvailable = true;
+            Log.d("Accelerometer", "Accelerometer is initialized!");
+        } else {
+            isAccelerometerSensorAvailable = false;
+            Log.d("Accelerometer", "Accelerometer is not initialized!");
+        }
 
         return paintCanvas;
         //PaintCanvas(Context context, AttributeSet attrs, GestureDetector mGestureDetector)
@@ -64,6 +90,61 @@ public class CanvasFragment extends Fragment {
     }
 
     @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        currentX = sensorEvent.values[0];
+        currentY = sensorEvent.values[1];
+        currentZ = sensorEvent.values[2];
+
+        if(itIsNotFirstTime){
+            xDifference = Math.abs(lastX - currentX);
+            yDifference = Math.abs(lastY - currentY);
+            zDifference = Math.abs(lastZ - currentZ);
+
+            // Need to check if any of these 3 has a value more than 5. If so, we will vibrate the phone
+            if((xDifference > shakeThreshold && yDifference > shakeThreshold) ||
+               (xDifference > shakeThreshold && zDifference > shakeThreshold) ||
+               (yDifference > shakeThreshold && zDifference > shakeThreshold)){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+                    paintCanvas.canvasErase();
+                } else {
+                    vibrator.vibrate(1000);
+                    //
+                }
+
+            }
+        }
+
+        lastX = currentX;
+        lastY = currentY;
+        lastZ = currentZ;
+        itIsNotFirstTime = true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (isAccelerometerSensorAvailable){
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (!isAccelerometerSensorAvailable){
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
     public void onSaveInstanceState(final @NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         final Parcelable[] canvasLines = paintCanvas.lines.toArray(new PaintCanvas.Line[0]);
@@ -74,7 +155,6 @@ public class CanvasFragment extends Fragment {
         paintCanvas.changeColor(color);
     }
 
-
     private static class PaintCanvas extends View implements View.OnTouchListener {
 
         private Deque<Line> lines = new LinkedList<>();
@@ -84,13 +164,6 @@ public class CanvasFragment extends Fragment {
         private int currentPaintColor = Color.BLACK;
 
         private GestureDetector mGestureDetector;
-
-        /*public PaintCanvas(Context context) {
-            super(context);
-            setOnTouchListener(this);
-            setBackgroundColor(backGroundColor);
-            initPaint();
-        }*/
 
         public PaintCanvas(Context context, AttributeSet attrs, GestureDetector mGestureDetector) {
             super(context, attrs);
@@ -166,10 +239,6 @@ public class CanvasFragment extends Fragment {
             Random r = new Random();
             backGroundColor = Color.rgb(r.nextInt(256), r.nextInt(256), r.nextInt(256));
             setBackgroundColor(backGroundColor);
-        }
-
-        public void changeStrokeSize() {
-            currentLine.getPaint().setStrokeWidth(50f);
         }
 
         public void canvasErase() {
@@ -265,7 +334,6 @@ public class CanvasFragment extends Fragment {
         @Override
         public boolean onSingleTapConfirmed(MotionEvent e) {
             Log.i("SingleTap", "Single Tap!");
-            //canvas.changeStrokeSize();
             return false;
         }
 
